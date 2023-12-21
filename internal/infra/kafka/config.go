32d4f5caa91f28357ece3a5b6f3586db3da2b504
caard0s0 <vinicardoso216@gmail.com>
2023-12-21 10:53:02 -0300

    chore: consumer group id changed

--- internal/infra/kafka/config.go
+++ internal/infra/kafka/config.go
@@ -5,7 +5,7 @@ import ckafka "github.com/confluentinc/confluent-kafka-go/v2/kafka"
 func ConsumerConfigMap() *ckafka.ConfigMap {
 	return &ckafka.ConfigMap{
 		"bootstrap.servers": "host.docker.internal:9094",
-		"group.id":          "myGroup",
+		"group.id":          "ConsumerGroup_1",
 		"auto.offset.reset": "earliest",
 	}
 }
